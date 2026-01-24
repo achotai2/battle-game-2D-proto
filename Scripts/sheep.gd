3048ab027d2fd457446fb298969a94a9186f5e66
achotai2 <achotai2@gmail.com>
2026-01-24 23:01:15 +0800

    Added agent nodes to sheep.

--- Scripts/sheep.gd
+++ Scripts/sheep.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@export var movement: AgentMovement
+@export var pathfinding: MinionPathfinding
+@export var animate: AgentAnimate
 @export var despawn_timer: Timer = null
 @export var move_speed: float = 50.0
 @export var hop_radius: float = 100.0
